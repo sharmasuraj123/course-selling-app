947c5b6e98251c423f2d257a546c8820e7dab7d2
sharmasuraj123 <surajsharma9602636265@gmail.com>
2025-09-07 09:44:08 +0000

    adding readme and gitignore file and initialize the routes

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const app = express();
+
+app.post('/user/signup',(req,res)=>{
+
+})
+
+app.post('/user/login',(req,res)=>{
+    
+})
+
+app.get('/courses',(req,res)=>{
+    
+})
+
+app.get('/user/purchases',(res,res)=>{
+    
+})
+
+app.post('/user/perchase',(req,res)=>{
+    
+})
+
+
+app.listen(4000,()=>{
+    console.log("the server is listent on port 4000");
+})
